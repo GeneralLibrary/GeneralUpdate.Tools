a1bb9a6d18bc33124ca36430bf0149c9d6ccf641
Juster.zhu <zhuzhen723723@163.com>
2023-03-03 22:58:03 +0800

    code clean

--- src/c#/GeneralUpdate.PacketTool/Platforms/Windows/FolderPicker.cs
+++ src/c#/GeneralUpdate.PacketTool/Platforms/Windows/FolderPicker.cs
@@ -10,15 +10,11 @@ namespace GeneralUpdate.PacketTool.Platforms.Windows
             var folderPicker = new WindowsFolderPicker();
             // Might be needed to make it work on Windows 10
             folderPicker.FileTypeFilter.Add("*");
-
             // Get the current window's HWND by passing in the Window object
             var hwnd = ((MauiWinUIWindow)App.Current.Windows[0].Handler.PlatformView).WindowHandle;
-
             // Associate the HWND with the file picker
             WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, hwnd);
-
             var result = await folderPicker.PickSingleFolderAsync();
-
             return result?.Path;
         }
     }
