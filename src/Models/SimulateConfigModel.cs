96c5fde7ee472b2395368a5e270b98f4192d7ff9
JusterZhu <zhuzhen723723@outlook.com>
2026-05-21 02:20:59 +0800

    revert: remove PR #58 (compile upgrade to .exe)
    - Remove CompileUpgrade checkbox and property
    - Remove .csproj/PublishUpgradeAsync
    - Restore simple ClientGeneratorService (upgrade.csx only)
    - Remove i18n keys

--- src/Models/SimulateConfigModel.cs
+++ src/Models/SimulateConfigModel.cs
@@ -16,6 +16,5 @@ public partial class SimulateConfigModel : ObservableObject
     [ObservableProperty] private string _appSecretKey = "dfeb5833-975e-4afb-88f1-6278ee9aeff6";
     [ObservableProperty] private string _productId = "2d974e2a-31e6-4887-9bb1-b4689e98c77a";
     [ObservableProperty] private string _outputDirectory = string.Empty;
-    [ObservableProperty] private bool _compileUpgrade = true;
     public int ServerPort { get; set; } = 5000;
 }
